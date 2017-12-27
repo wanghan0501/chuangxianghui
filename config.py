37135d43edbc5edb61583505446b3fca8997b3f8
王晗 <hanwang.0501@gmail.com>
2017-12-27 16:04:55 +0800

    Frame

---
+++ config.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin/env python
+
+"""
+Created by Wang Han on 27/12/2017 15:23.
+E-mail address is hanwang.0501@gmail.com.
+Copyright © 2017 Wang Han. SCU. All right Reserved.
+"""
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    key = ''
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+config = {
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig,
+}
+
